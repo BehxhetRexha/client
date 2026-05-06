6500b41e90e4932f2f8aac2b2403fabebcbffe36
Behxhet <behxhetrexha66@gmail.com>
2026-05-06 17:13:37 +0200

    part6

---
+++ src/types/user.ts
@@ -0,0 +1,18 @@
+export type User = {
+id:string;
+displetName:string;
+email:string;
+token:string;
+imageUrl:string;
+}
+
+export type LoginCreds = {
+email:string;
+password:string;
+}
+
+export type RegisterCreds = {
+displetName:string;
+email:string;
+password:string;
+}
